c6ad5561a758019f08521a03126921ab827a5e92
Kai Zheng <59148999+kaiz715@users.noreply.github.com>
2025-05-09 11:24:23 -0400

    Initial Commit

---
+++ Calorix/CalorixApp.swift
@@ -0,0 +1,32 @@
+//
+//  CalorixApp.swift
+//  Calorix
+//
+//  Created by Kai Zheng on 5/9/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CalorixApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CalorixTests/CalorixTests.swift
@@ -0,0 +1,17 @@
+//
+//  CalorixTests.swift
+//  CalorixTests
+//
+//  Created by Kai Zheng on 5/9/25.
+//
+
+import Testing
+@testable import Calorix
+
+struct CalorixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
